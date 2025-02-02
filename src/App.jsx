efd17640ba925ff2ed574b50fbe96b9673975bbb
Matt Mattox <bizofmatt@gmail.com>
2025-02-01 17:47:52 -0800

    configured tailwind css for project

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="font-playfair text-5xl text-blue-500 underline">
         The beginning of Wave
+      </div>
+      <div className="font-lato text-xl bg-emerald-300">
+        This is lato font
       </div>
     </>
   )
