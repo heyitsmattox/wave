57ba7b924f2faf1d8d1b71f952d90393ef6e2007
Matt Mattox <bizofmatt@gmail.com>
2025-02-07 21:43:53 -0800

    added mobile responsiveness to signInPage

--- src/pages/auth/FormContainer.jsx
+++ src/pages/auth/FormContainer.jsx
@@ -1,14 +1,21 @@
+import clsx from 'clsx';
+
 const FormContainer = (props) => {
   const { children } = props;
   return (
+
+    //TODO - MOBILE RESPONSIVENESS
     <>
       <div className="bg-stone-100 flex items-center justify-center">
         {/* <div className="w-full bg-blue-800 opacity-80 object-cover h-screen"></div> */}
-        
+        <div className='w-[1200px] relative hidden md:flex '>
           <img 
-          className="bg-black  w-full object-cover h-screen"
+          className="bg-black object-cover h-screen w-full"
           src="/images/homescreen.png" 
           alt="wave"/>
+
+
+        </div>
 
         <div className="w-full flex flex-col items-center h-screen justify-center">
           <div className="mt-10 my-4 flex items-center justify-center">
