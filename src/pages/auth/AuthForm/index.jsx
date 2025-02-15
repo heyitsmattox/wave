913c6449b49ea25cae9b5ce8fc6d38c232921d08
Matt Mattox <bizofmatt@gmail.com>
2025-02-15 14:32:41 -0800

    Added LoadingSpinner shared-component

--- src/pages/auth/AuthForm/index.jsx
+++ src/pages/auth/AuthForm/index.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Field from "./Field";
-
-// import * as userService from "../../../services/users";
+import LoadingSpinner from "../../../shared-components/LoadingSpinner";
 
 const AuthForm = (props) => {
-
   const { fields, submitButtonLabel, onSubmit } = props;
-
+  const [loading, setLoading] = useState(null);
 
   //keep track of the input the user is doing each time
   const [fieldValues, setFieldValues] = useState(() => {
@@ -20,49 +18,51 @@ const AuthForm = (props) => {
     return initialState;
   });
 
-
-
   return (
     <>
-    <form className="font-lato m-4 p-4 bg-white border border-slate-300 rounded-lg"
-    onSubmit={async (e) => {
-      e.preventDefault(); 
-      if (onSubmit) {
-        await onSubmit(fieldValues);
-        console.log("location AuthForm: --> Form submitted");
-      } else {
-        console.error("Error submitting form!");
-      }
-    }}
-
-    >
-      {fields.map((field) => (
-        <Field
-        key={field.label}
-          label={field.label}
-          type={field.type}
-          value={fieldValues[field.label]}
-          onChange={(e) => {
-            //keep our object reference
-            setFieldValues({
-              //copying over our original values
-              //assign our new values
-              ...fieldValues,
-              [field.label]: e.target.value,
-            });
-          }}
-        />
-      ))}
-      <button
-      type="submit"
-        className="bg-blue-700 text-white p-4 w-full rounded-lg 
-py-3 shadow-md mt-4"
-        onSubmit={fieldValues}
+      <form
+        className="font-lato m-4 p-4 bg-white border border-slate-300 rounded-lg"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          if (onSubmit) {
+            setLoading(true);
+            await onSubmit(fieldValues);
+            console.log("location AuthForm: --> Form submitted");
+            setLoading(false);
+          } else {
+            console.error("Error submitting form!");
+          }
+        }}
       >
-        {submitButtonLabel}
-      </button>
-    </form>
-    
+        {fields.map((field) => (
+          <Field
+            key={field.label}
+            label={field.label}
+            type={field.type}
+            value={fieldValues[field.label]}
+            onChange={(e) => {
+              //keep our object reference
+              setFieldValues({
+                //copying over our original values
+                //assign our new values
+                ...fieldValues,
+                [field.label]: e.target.value,
+              });
+            }}
+          />
+        ))}
+        <button
+          type="submit"
+          className="relative flex justify-center items-center bg-blue-700 text-white p-5 w-full rounded-lg 
+py-3 shadow-md mt-4"
+          onSubmit={fieldValues}
+        >
+          {submitButtonLabel}
+          <span className="w-5 h-5 inline-flex items-center justify-center">
+    {loading && <LoadingSpinner />}
+  </span>
+        </button>
+      </form>
     </>
   );
 };
---
+++ src/shared-components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+const LoadingSpinner = () => {
+  return (
+    <>
+      <div className="flex justify-center text-center">
+        <i className="text-lg text-white fa-solid fa-spinner animate-spin ml-3"></i>
+      </div>
+    </>
+  );
+};
+
+export default LoadingSpinner;
