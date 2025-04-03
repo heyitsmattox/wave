045176e77f114853206a10a977b39bc9f102c8e7
Matt Mattox <bizofmatt@gmail.com>
2025-04-03 07:35:23 -0700

    placeholder text is dynamic based on window's size

--- src/pages/Product-Search-Page/SearchBar.jsx
+++ src/pages/Product-Search-Page/SearchBar.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [placeholder, setPlaceholder ] = useState("");
   const navigate = useNavigate();
 
   const handleSearch = () => {
@@ -11,6 +12,23 @@ const SearchBar = () => {
     if (!searchQuery.trim()) return;
     navigate(`/search?q=${encodeURIComponent(searchQuery)}`);
   };
+
+  useEffect(() => {
+   const handleResize = () => {
+    if(window.innerWidth < 750) {
+      setPlaceholder("Search")
+    } else {
+      setPlaceholder("Search for a product")
+    }
+   } 
+   handleResize();
+
+   window.addEventListener('resize', handleResize);
+   return () => window.removeEventListener('resize', handleResize);
+
+  }, [])
+
+
 
   return (
     <div className="flex justify-center p-8">
@@ -20,7 +38,7 @@ const SearchBar = () => {
           type="text"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          placeholder="Search for a product"
+          placeholder={placeholder}
           className="max-md:!w-full shadow-lg max-w-96 p-2 pl-8 rounded-md border border-slate-400"
         />
         <i
