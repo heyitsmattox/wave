070ad47319ea350328bd720839d91eb6a0f622fd
Matt Mattox <bizofmatt@gmail.com>
2025-04-11 18:45:10 -0700

    updated pool to db for readability

--- backend/modules/fetchUsers.js
+++ backend/modules/fetchUsers.js
@@ -1,8 +1,8 @@
-import pool from "../database/db.js";
+import db from "../database/db.js";
 
 async function fetchUsers() {
   try {
-    const { rows } = await pool.query('SELECT * FROM users');
+    const { rows } = await db.query('SELECT * FROM users');
     console.log("✅ Users in table:", rows);
   } catch (err) {
     console.error("❌ Failed to fetch users:", err);
