a259e14fc4a5f56cb965cf13f8435def8ebf6601
Matt Mattox <bizofmatt@gmail.com>
2025-04-11 18:59:00 -0700

    controller not middleware on previous commit

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -11,7 +11,6 @@ const usersController =  {
   }
 
 
-
   // createUsers: async (req, res) => {
   //   const { id, email, password } = req.body;
   //   const query = `INSERT INTO users (id, email, password) VALUES ($1, $2, $3)`
