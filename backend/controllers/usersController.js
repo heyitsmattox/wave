1e2e79f8c191b14acc5a56d08939076e94ba98d1
Matt Mattox <bizofmatt@gmail.com>
2025-04-11 18:56:14 -0700

    moved fetchUsers middleware function inside our usersController middleware

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,8 +1,21 @@
-import { db } from "../database/db";
+import db from "../database/db.js";
 
-// const usersController =  {
-//   createUsers: async (req, res) => {
-//     const { id, email, password } = req.body;
-//     const query = `INSERT INTO users (id, email, password) VALUES ($1, $2, $3)`
-//   }
-// }
+const usersController =  {
+  fetchUsers: async (req, res) => {
+    try {
+      const { rows } = await db.query('SELECT * FROM users');
+      console.log("✅ Users in table:", rows);
+    } catch (err) {
+      console.error("❌ Failed to fetch users:", err);
+    }
+  }
+
+
+
+  // createUsers: async (req, res) => {
+  //   const { id, email, password } = req.body;
+  //   const query = `INSERT INTO users (id, email, password) VALUES ($1, $2, $3)`
+  // }
+};
+
+export default usersController;
