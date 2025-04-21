b40f6ad0a90ba9ce3324e4437da828a887d5389c
Matt Mattox <bizofmatt@gmail.com>
2025-04-21 07:48:37 -0700

    navbar menu dropdown prototype

--- src/shared-components/Navbar.jsx
+++ src/shared-components/Navbar.jsx
@@ -1,6 +1,13 @@
 import SignOutBtn from "./SignOutBtn";
+import { useState  } from "react";
 
 const Navbar = () => {
+  const [ isClicked, setIsClicked ] = useState(false);
+
+  const handleMenuBtn = () => {
+    setIsClicked(true);
+  }
+
   return (
     <>
       <div className="pt-8 w-full border-b border-indigo-400 p-4">
@@ -8,7 +15,17 @@ const Navbar = () => {
           <i class="fa-solid text-3xl fa-wave-square flex text-amber-400">
             <div className="text-3xl font-lato ml-4 text-indigo-400">Wave</div>
           </i>
-          <i className="fa-solid text-xl fa-bars text-slate-500"></i>
+          <button 
+          onClick={handleMenuBtn}
+          className="fa-solid text-xl fa-bars text-slate-500">
+            {
+              isClicked && (
+                <div className="bg-blue-400 p-10 h-10">
+
+                </div>
+              )
+            }
+          </button>
         </div>
       </div>
     </>
